6de6d97fde4e7748d28a660888c6ba732209249d
Nwani Victory <vickywane@gmail.com>
2021-07-18 20:51:03 +0100

    Delete credentials.js

--- src/credentials.js
+++
@@ -1,2 +0,0 @@
-export const REALM_GRAPHQL_ENDPOINT = "https://us-east-1.aws.realm.mongodb.com/api/client/v2.0/app/offline-egghead-app-hrgft/graphql"
-export const  REALM_ID="offline-egghead-app-hrgft"
